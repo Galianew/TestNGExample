c6a82795c500c3408bcfb20cdae897852811403a
Galianew <galia.dziubenko@gmail.com>
2016-04-28 20:16:50 +0300

    Initial commit

---
+++ src/test/java/TestNG.java
@@ -0,0 +1,73 @@
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.testng.annotations.*;
+
+/**
+ * Created by Admin on 28.04.2016.
+ */
+public class TestNG {
+    @BeforeGroups("shopping")
+    public void beforeGroups(){
+        System.out.println("@Beforegroups");
+    }
+
+    @AfterGroups("shopping")
+    public void afterGroups() {
+        System.out.println("@AfterGroups");
+    }
+
+    @BeforeClass
+    public void beforeClass() {
+        System.out.println("@BeforeClass");
+    }
+
+    @AfterClass
+    public void afterClass() {
+        System.out.println("@AfterClass");
+    }
+
+    @Test(priority=3)
+    public void runTest1() {
+        System.out.println("Test1 -groups shopping");
+    }
+
+    @Test(priority=1)
+    public void runTest2() {
+        System.out.println("Test2 -no groups");
+    }
+
+    @Test(priority=4)
+    public void runTest3() {
+        System.out.println("Test3 - groups merch");
+    }
+
+    @BeforeSuite
+    public void beforeSuite() {
+        System.out.println("@BeforeSuite");
+    }
+
+    @BeforeTest
+    public void beforeTest(){
+        System.out.println("@BeforeTest");
+    }
+
+    @AfterTest
+    public void afterTest(){
+        System.out.println("@AfterTest");
+    }
+
+    @AfterSuite
+    public void afterSuite() {
+        System.out.println("@AfterSuite");
+    }
+
+    @BeforeMethod
+    public void beforeMethod(){
+        System.out.println("@BeforeMetod");
+    }
+
+    @AfterMethod
+    public void afterMethod(){
+        System.out.println("@AfterMetod");
+    }
+}
